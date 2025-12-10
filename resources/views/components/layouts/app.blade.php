54fde68667dafb316d3aac1059340b9bddb651d6
lbirchenough <luke.birchenough@outlook.com>
2025-12-10 23:16:26 +0800

    Removing second argument from @vite('resources/css/app.css', 'resources/js/app.js') as it was setting a different path for manifest

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,8 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        @vite('resources/css/app.css', 'resources/js/app.js')
-        @livewireStyles
+        @vite('resources/css/app.css')
         {{-- Chart.js  --}}
         <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
         <title>{{ $title ?? 'Page Title' }}</title>
